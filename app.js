8d3e21bc5b06c49568e8b53a981320cdbb931c07
Aljaž Avguštin <60004471+aljazavgustin@users.noreply.github.com>
2022-07-06 12:01:02 +0200

    feat
    Mreža za ugibanje besed, styling, funkcija ki doda prvo črko, ki jo pritisnemo v polje v prvi vrstici

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const title = document.querySelector(".title-container");
 const keyboard = document.querySelector(".keyboard-container");
+const tileDisplay = document.querySelector(".tile-container");
 
 const keys = [
   "Q",
@@ -32,15 +33,57 @@ const keys = [
   "<<", //BAKCSPACE
 ];
 
+const guesses = [
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+];
+
+/* Trenutna vrstica */
+let currentRow = 0;
+/* Trenutna pozicija v vrstici */
+let currentTile = 0;
+
+
+/* Naredimo mrežo s polja guesses. Kjer je za vsaki element v polju narejen div,
+ki še nato za vsak prazen string v polju naredi nov div */
+guesses.forEach((guessRow, guessRowIndex) => {
+  const rowElement = document.createElement("div");
+  rowElement.setAttribute("id", "guessRow-" + guessRowIndex);
+  guessRow.forEach((guess, guessIndex) => {
+    const tileElement = document.createElement("div");
+    tileElement.setAttribute(
+      "id",
+      "guessRow-" + guessRowIndex + "-tile-" + guessIndex
+    );
+    tileElement.classList.add("tile");
+    rowElement.append(tileElement);
+  });
+  tileDisplay.appendChild(rowElement);
+});
+
 /* Z vsake črke/znaka v polju naredimo button element in 
-dodamo v div (.keyboard-container)
-*/
+dodamo v div (.keyboard-container) q*/
 keys.forEach((key) => {
   const keyElement = document.createElement("button");
   keyElement.textContent = key;
   keyElement.setAttribute("id", key);
   keyElement.addEventListener("click", () => {
-    console.log("Klik");
+    handleClick(key);
   });
   keyboard.append(keyElement);
 });
+
+const handleClick = (pressedKey) => {
+  console.log('Kliknil si', pressedKey);
+  addLetter(pressedKey);
+}
+
+/* Funkcija, ki doda črko v polje */
+const addLetter = (letter) => {
+  const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile);
+  tile.textContent = letter;
+}
