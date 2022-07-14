2ff7a5da2c9e506b19fef074304d44799f23fcc1
Aljaž Avguštin <60004471+aljazavgustin@users.noreply.github.com>
2022-07-14 11:35:28 +0200

    feat
    added delete letter, check answer and game message

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const title = document.querySelector(".title-container");
 const keyboard = document.querySelector(".keyboard-container");
 const tileDisplay = document.querySelector(".tile-container");
+const messageDisplay = document.querySelector(".message-container");
 
 const keys = [
   "Q",
@@ -42,11 +43,14 @@ const guesses = [
   ["", "", "", "", ""],
 ];
 
+/* Spremenljivka za state, kjer preverjamo, če je konec igre */
+let isGameOver = false;
+/* Beseda za ugibanje */
+let wordle = "ADIJO";
 /* Trenutna vrstica */
 let currentRow = 0;
 /* Trenutna pozicija v vrstici */
 let currentTile = 0;
-
 
 /* Naredimo mrežo s polja guesses. Kjer je za vsaki element v polju narejen div,
 ki še nato za vsak prazen string v polju naredi nov div */
@@ -78,12 +82,69 @@ keys.forEach((key) => {
 });
 
 const handleClick = (pressedKey) => {
-  console.log('Kliknil si', pressedKey);
-  addLetter(pressedKey);
-}
+  if (pressedKey === "<<") {
+      deleteLetter()
+      return
+  }
+  if (pressedKey === "ENTER") {
+    checkGuess()
+    return
+  }
+  if (currentTile < 5 && currentRow < 6) {
+    console.log("Kliknil si", pressedKey);
+    addLetter(pressedKey);
+  }
+};
 
 /* Funkcija, ki doda črko v polje */
 const addLetter = (letter) => {
-  const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile);
+  const tile = document.getElementById(
+    "guessRow-" + currentRow + "-tile-" + currentTile
+  );
   tile.textContent = letter;
+  guesses[currentRow][currentTile] = letter;
+  tile.setAttribute("data", letter);
+  currentTile++;
+};
+
+/* Funkcija, ki zbriše črko v prešnjem polju oz. nastavi vrednost na "" */
+const deleteLetter = () => {
+  if (currentTile > 0) {
+    currentTile--;
+    const tile = document.getElementById(
+      "guessRow-" + currentRow + "-tile-" + currentTile
+    );
+    tile.textContent = "";
+    guesses[currentRow][currentTile] = "";
+    tile.setAttribute("data", "");
+  }
+}
+
+/* Funkcija, ki preveri vrstico z izbrano besedo */
+const checkGuess = () => {
+  if (currentTile === 5) {
+    const guess = guesses[currentRow].join('');
+    if (guess == wordle) {
+      showMessage('Odlično!');
+    } else {
+      if (currentRow >= 5) {
+        isGameOver = true;
+        showMessage('Konec igre!');
+        return
+      }
+      if (currentRow < 5) {
+        currentRow++;
+        currentTile = 0;
+      }
+    }
+  }
+}
+
+const showMessage = (message) => {
+  const messageElement = document.createElement('p');
+  messageElement.textContent = message;
+  messageDisplay.append(messageElement);
+  setTimeout(() => {
+    messageDisplay.removeChild(messageElement);
+  }, 2000);
 }
